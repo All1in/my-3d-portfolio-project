9596b20b49e8ac2b917190e1a57b63ba6d91b101
All1in <bm3023068@gmail.com>
2024-01-02 14:38:30 +0200

    navbar component implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar.jsx";
 
 const App = () => (
-   <main>
-       <h1 className="text-3xl font-bold underline">
-           Hello world!
-       </h1>
+   <main className='bg-slate-300/20'>
+       <Router>
+           <Navbar />
+           <Routes>
+               {/*<Route path='/' element={<Home />} />*/}
+               <Route
+                   path='/*'
+                   element={
+                       <>
+                           <Routes>
+                               {/*<Route path='/about' element={<About />} />*/}
+                               {/*<Route path='/projects' element={<Projects />} />*/}
+                               {/*<Route path='/contact' element={<Contact />} />*/}
+                           </Routes>
+                           {/*<Footer />*/}
+                       </>
+                   }
+               />
+           </Routes>
+       </Router>
    </main>
 );
 
