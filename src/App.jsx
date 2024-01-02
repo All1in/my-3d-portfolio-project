94007bde1d4d0ab56f644a2a86efb85f5593c068
All1in <bm3023068@gmail.com>
2024-01-02 14:20:41 +0200

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+const App = () => (
+   <main>
+       <h1 className="text-3xl font-bold underline">
+           Hello world!
+       </h1>
+   </main>
+);
+
+export default App;
