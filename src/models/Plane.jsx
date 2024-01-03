420e8f53e93046b38aa05aede50b2d0f1d20bf9f
All1in <bm3023068@gmail.com>
2024-01-03 14:22:27 +0200

    bird & plane component implemented

---
+++ src/models/Plane.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useRef } from "react";
+import { useGLTF, useAnimations } from "@react-three/drei";
+
+import planeScene from "../assets/3d/plane.glb";
+
+export function Plane({ isRotating, ...props }) {
+    const ref = useRef();
+    // Load the 3D model and its animations
+    const { scene, animations } = useGLTF(planeScene);
+    // Get animation actions associated with the plane
+    const { actions } = useAnimations(animations, ref);
+
+    // Use an effect to control the plane's animation based on 'isRotating'
+    // Note: Animation names can be found on the Sketchfab website where the 3D model is hosted.
+    useEffect(() => {
+        if (isRotating) {
+            actions["Take 001"].play();
+        } else {
+            actions["Take 001"].stop();
+        }
+    }, [actions, isRotating]);
+
+    return (
+        <mesh {...props} ref={ref}>
+            model or scene
+            <primitive object={scene} />
+        </mesh>
+    );
+}
